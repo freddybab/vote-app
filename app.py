980fbe2a6dec1da6e2a9402facf3bc8f8b00ba95
Andreas Dørum <andreas.doerum@gmail.com>
2022-03-29 09:50:07 +0200

    add database tasks

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
+import contextlib
 import os
+import sqlite3
 
 from flask import Flask, request, send_from_directory
 from werkzeug.utils import safe_join
-from flask_restx import Api, Resource, fields
+from flask_restx import Api, Resource, fields, marshal
 
 static = safe_join(os.path.dirname(__file__), 'static')
 
 app = Flask(__name__)
+
+DATABASE_FILE = 'database.db'
 
 @app.route('/')
 def _home():
@@ -31,38 +35,97 @@ todo = api.model('Todo', {
 })
 
 
-class TodoDAO(object):
+class TodoDAO(object):    
     def __init__(self):
-        self.counter = 0
-        self.todos = []
+        self.setup_database()
 
+    """
+    Kjører SQL-spørringen 'statement' med gitte verdi-bindinger 'values'
+    """
+    def _execute_sql(self, statement, values):
+        with contextlib.closing(sqlite3.connect(DATABASE_FILE)) as conn: # auto-closes
+            with conn: # auto-commits
+                with contextlib.closing(conn.cursor()) as cursor: # auto-closes
+                    cursor.execute(statement, values)
+                    fetch = cursor.fetchall()
+                    lastrow = cursor.lastrowid
+                    return fetch, lastrow         
+
+    
+    """
+    Kjører SQL-spørringen om returnere alt som matcher, bruk til SELECT.
+    """
+    def _execute_sql_fetchall(self, statement, values):
+        fetch, _ = self._execute_sql(statement, values)
+        return fetch
+
+    """
+    Kjører SQL-spørringen og returnerer iden til siste rad. Bruk til INSERT.
+    """
+    def _execute_sql_lastrowid(self, statement, values):
+        _, lastrow = self._execute_sql(statement, values)
+        return lastrow
+
+
+    def _map_todo(self, todo_row):
+        return {
+                'id': todo_row[0],
+                'task': todo_row[1],
+                'fav': todo_row[2]
+            }
+
+    """
+    Opprett tabeller i databasen om database-filen ikke finnes fra før.
+    """
+    def setup_database(self):
+        if not os.exists(DATABASE_FILE):
+            # OPPGAVE: Skriv SQL som oppretter en tabell med feltene i en todo:
+
+                self._execute_sql('''CREATE TABLE ...
+            )''', {})
+    
+    def get_all(self):
+        return_list = []
+        # OPPGAVE: Skriv SQL som hentesr alle radene i todo tabellen
+
+        todos = self._execute_sql_fetchall('''
+        SELECT ...
+        ''', {}) # todo convert to json? marshal? ORM?
+        for todo in todos:
+            return_list.append(self.map_todo(todo))
+        return return_list
+    
     def get(self, id):
-        for todo in self.todos:
-            if todo['id'] == id:
-                return todo
-        api.abort(404, "Todo {} doesn't exist".format(id))
+
+        # OPPGAVE: Skriv SQL som henter todo-raden med den gitte id-en
+        todo = self._execute_sql('''
+        SELECT ...''', {'id': id})
+        return self.map_todo(todo)
 
     def create(self, data):
         todo = data
-        self.counter += 1
-        todo['id'] = self.counter
-        self.todos.append(todo)
+        # OPPGAVE: Skriv SQL som setter inn en ny rad i todo tabellen
+        todo_id = self._execute_sql_lastrowid('''
+        INSERT ...
+        ''', data)
+
+        todo['id'] = todo_id
         return todo
 
     def update(self, id, data):
-        todo = self.get(id)
-        todo.update(data)
+        # OPPGAVE: Skriv SQL som oppdaterer den gitte raden i tabellen
+        self._execute_sql('''
+        UPDATE ...
+        ''', data)
         return todo
 
     def delete(self, id):
-        todo = self.get(id)
-        self.todos.remove(todo)
-
+        # OPPGAVE: Skriv SQL som sletter den gitte raden i tabellen
+        self._execute_sql('''
+        DELETE ...
+        ''', {'id': id})
 
 DAO = TodoDAO()
-DAO.create({'task': 'Build an API'})
-DAO.create({'task': '?????'})
-DAO.create({'task': 'profit!'})
 
 @ns.route('/')
 class TodoList(Resource):
@@ -71,14 +134,15 @@ class TodoList(Resource):
     @ns.marshal_list_with(todo)
     def get(self):
         '''List all tasks'''
-        return DAO.todos
+        return DAO.get_all()
 
     @ns.doc('create_todo')
     @ns.expect(todo)
     @ns.marshal_with(todo, code=201)
     def post(self):
         '''Create a new task'''
-        return DAO.create(api.payload), 201
+        marshalled = marshal(api.payload, todo)
+        return DAO.create(marshalled), 201
 
 
 @ns.route('/<int:id>')
